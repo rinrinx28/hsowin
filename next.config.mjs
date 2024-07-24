8ef4cb4553108d061517d6c30065b599229e76c5
Rinx28 <minhanhpreeip@gmail.com>
2024-07-24 20:51:14 +0700

    Change images and change out > dist

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@
  */
 const nextConfig = {
 	output: 'export',
+	images: { unoptimized: true },
 
 	// Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
 	// trailingSlash: true,
@@ -11,7 +12,7 @@ const nextConfig = {
 	// skipTrailingSlashRedirect: true,
 
 	// Optional: Change the output directory `out` -> `dist`
-	// distDir: 'dist',
+	distDir: 'dist',
 };
 
 export default nextConfig;
