c845726bcdb2ed6629d58bd1ceda98b5f92fbb12
Rinx28 <minhanhpreeip@gmail.com>
2024-07-31 23:21:16 +0700

    Change transport

--- src/lib/socket.tsx
+++ src/lib/socket.tsx
@@ -11,8 +11,8 @@ const urlConfig = {
 };
 
 const socket: Socket = io(urlConfig.https, {
-	transports: ['websocket', 'polling'],
-	withCredentials: true,
+	transports: ['websocket'],
+	// withCredentials: true,
 	reconnectionAttempts: 5, // Limit reconnection attempts
 	// auth: {
 	//   token: 'your-auth-token' // Ensure to pass a valid token
