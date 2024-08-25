f14ab1ce64312ce383c80cf31f10284f433c9a85
Rinx28 <minhanhpreeip@gmail.com>
2024-08-26 03:28:28 +0700

    Change domain Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<footer className="footer footer-center bg-base-300 text-base-content p-4">
+		<footer className="footer footer-center bg-base-300 lg:text-base-content text-sm p-4">
 			<aside>
 				<p>
-					Copyright © {new Date().getFullYear()} - All right reserved by HSOWIN
-					VIP
+					Copyright © {new Date().getFullYear()} - All right reserved by
+					HSGAME.ME
 				</p>
 			</aside>
 		</footer>
